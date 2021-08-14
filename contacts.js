2d40f5afcc2935e6438666afcbba0f3dd23bc97f
Oleksii Patsiuk <patsyuck@ukr.net>
2021-08-14 09:35:21 +0300

    Step 2 added

--- contacts.js
+++ contacts.js
@@ -1,11 +1,13 @@
 const fs = require('fs/promises')
-const path =
-    
-const contactsPath =
+const path = require('path')
+const {v4} = require('uuid')
+const contactsPath = path.join(__dirname, 'db/contact.json')
 
 async function listContacts() {
     try {
-        const data = await fs.readFile(contactsPath, "utf-8")
+        const data = await fs.readFile(contactsPath)
+        result = JSON.parse(data)
+        console.table(result)
     }
     catch (error) {
         console.log(error.message)
@@ -13,21 +15,59 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-    try { }
+    try {
+        const data = await fs.readFile(contactsPath)
+        result = JSON.parse(data)
+        const contact = result.find(item => item.id === contactId)
+        if (!contact) {
+            throw new Error(`Contact with id=${contactId} not found!`)
+        }
+        console.log(contact)
+    }
     catch (error) {
         console.log(error.message)
     }
 }
 
 async function removeContact(contactId) {
-    try { }
+    try {
+        const data = await fs.readFile(contactsPath)
+        result = JSON.parse(data)
+        const idx = result.findIndex(item => item.id === contactId)
+        if (idx === -1) {
+            throw new Error(`Contact with id=${contactId} not found!`)
+        }
+        const contact = result.find(item => item.id === contactId)
+        const contacts = result.filter(item => item.id !== contactId)
+        const newContacts = JSON.stringify(contacts)
+        await fs.writeFile(contactsPath, newContacts)
+        console.log('One contact removed:')
+        console.log(contact)
+    }
     catch (error) {
         console.log(error.message)
     }
 }
 
 async function addContact(name, email, phone) {
-    try { }
+    try {
+        const data = await fs.readFile(contactsPath)
+        result = JSON.parse(data)
+        const idx = Math.max(...result.map(item => item.id)) + 1 //?
+        const newContact = {
+            'id': idx, //?
+            //'id': v4(),
+            'name': name,
+            'email': email,
+            'phone': phone
+        }
+        result[result.length] = newContact //?
+        //result.push(newContact)
+        const newContacts = JSON.stringify(result)
+        await fs.writeFile(contactsPath, newContacts)
+        console.log('New contact added:')
+        console.log(newContact)
+    }
     catch (error) {
         console.log(error.message)
     }
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const contactsFunctions = require('./contacts')
+
+//contactsFunctions.getContactById(1)
+//contactsFunctions.addContact('Mango', 'mango@gmail.com', '322-22-22')
+//contactsFunctions.removeContact(21)
+
+//contactsFunctions.listContacts()
