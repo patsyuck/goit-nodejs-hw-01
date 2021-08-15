f598c932deb441ddaaf3adffdbb6d0191a2abac9
Oleksii Patsiuk <patsyuck@ukr.net>
2021-08-15 10:17:33 +0300

    Edits added

--- contacts.js
+++ contacts.js
@@ -2,10 +2,15 @@ const fs = require('fs/promises')
 const path = require('path')
 const contactsPath = path.join(__dirname, 'db/contact.json')
 
+async function readData() {
+    const data = await fs.readFile(contactsPath)
+    const result = JSON.parse(data)
+    return result
+}
+
 async function listContacts() {
     try {
-        const data = await fs.readFile(contactsPath)
-        result = JSON.parse(data)
+        const result = await readData()
         console.table(result)
     }
     catch (error) {
@@ -15,8 +20,7 @@ async function listContacts() {
 
 async function getContactById(contactId) {
     try {
-        const data = await fs.readFile(contactsPath)
-        result = JSON.parse(data)
+        const result = await readData()
         const contact = result.find(item => item.id === contactId)
         if (!contact) {
             throw new Error(`Contact with id=${contactId} not found!`)
@@ -30,13 +34,12 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
     try {
-        const data = await fs.readFile(contactsPath)
-        result = JSON.parse(data)
+        const result = await readData()
         const idx = result.findIndex(item => item.id === contactId)
         if (idx === -1) {
             throw new Error(`Contact with id=${contactId} not found!`)
         }
-        const contact = result.find(item => item.id === contactId)
+        const contact = result[idx]
         const contacts = result.filter(item => item.id !== contactId)
         const newContacts = JSON.stringify(contacts)
         await fs.writeFile(contactsPath, newContacts)
@@ -50,9 +53,8 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
     try {
-        const data = await fs.readFile(contactsPath)
-        result = JSON.parse(data)
-        const idx = Math.max(...result.map(item => item.id)) + 1 //?
+        const result = await readData()
+        const idx = result[result.length - 1]['id'] + 1
         const newContact = {
             'id': idx,
             'name': name,
